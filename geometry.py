e7f59cf32d2f57e0f4eee6c917e6b8a7168a73f0
Pratyush <pratyushkmr13@gmail.com>
2020-10-20 16:30:39 +0200

    fix return typo in distance func

--- geometry.py
+++ geometry.py
@@ -42,7 +42,7 @@ class Point(object):
     def distance(self, other):
         """Returns cartesian distance between self and other Point
         """
-        Returen( sqrt( (self.y - other.y)**2 + (self.x - other.x)**2) )
+        return( sqrt( (self.y - other.y)**2 + (self.x - other.x)**2) )
 
 
 class Circle(object):
